c7a52dc2ef12f6a3dd32d2fff9fee4c4c6c16f9f
Wataru Maeda <WataruMaeda@users.noreply.github.com>
2017-12-09 17:07:44 -0800

    init project

---
+++ video-en/ViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  video-en
+//
+//  Created by Wataru Maeda on 2017/12/09.
+//  Copyright © 2017 iosClassForBeginner. All rights reserved.
+//
+
+import UIKit
+import AVKit
+import AVFoundation
+
+class ViewController: UIViewController {
+  
+  @IBOutlet var playerView: UIView!
+  var player: AVPlayer!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    initPlayer()
+  }
+  
+  func initPlayer() {
+    let videoUrl = URL(string: "https://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4")
+    player = AVPlayer(url: videoUrl!)
+    let playerLayer = AVPlayerLayer(player: player)
+    playerLayer.frame = playerView.bounds
+    playerView.layer.addSublayer(playerLayer)
+  }
+  
+  @IBAction func play(_ sender: Any) {
+    player.play()
+  }
+  
+  @IBAction func stop(_ sender: Any) {
+    player.pause()
+  }
+  
+  @IBAction func retry(_ sender: Any) {
+    player.seek(to: kCMTimeZero)
+  }
+}
+
